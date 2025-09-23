65332dfb81531740c6b14e61de162429c07a9620
GlendyT <glendytuyuc@gmail.com>
2025-09-23 13:41:46 -0600

    creating the auth with zustnd, and adding the tabs of nativation

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,5 +1,6 @@
 import { images } from "@/constants";
-import { Slot } from "expo-router";
+import useAuthStore from "@/store/auth.store";
+import { Redirect, Slot } from "expo-router";
 import React from "react";
 import {
   Dimensions,
@@ -11,7 +12,10 @@ import {
   View,
 } from "react-native";
 
-const _Layout = () => {
+export default function AuthLayout() {
+  const { isAuthenticated } = useAuthStore();
+
+  if (isAuthenticated) return <Redirect href="/" />;
   return (
     <KeyboardAvoidingView
       behavior={Platform.OS === "ios" ? "padding" : "height"}
@@ -38,6 +42,4 @@ const _Layout = () => {
       </ScrollView>
     </KeyboardAvoidingView>
   );
-};
-
-export default _Layout;
+}
--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,10 +1,99 @@
-import { Redirect, Slot } from "expo-router";
+import { images } from "@/constants";
+import useAuthStore from "@/store/auth.store";
+import { TabBarIconProps } from "@/type";
+import { Redirect, Tabs } from "expo-router";
 import React from "react";
+import { Image, Text, View } from "react-native";
+import cn from "clsx";
 
-const _Layout = () => {
-  const isAuthenticated = false;
+const TabBarIcon = ({ focused, icon, title }: TabBarIconProps) => (
+  <View className="tab-icon">
+    <Image
+      source={icon}
+      className="size-7"
+      resizeMode="contain"
+      tintColor={focused ? "#FF8C00" : "#5d5f6d"}
+    />
+    <Text
+      className={cn(
+        "text-sm font-bold",
+        focused ? "text-primary" : "text-dark-200"
+      )}
+    >
+      {title}{" "}
+    </Text>
+  </View>
+);
+
+export default function TabLayout() {
+  const { isAuthenticated } = useAuthStore();
   if (!isAuthenticated) return <Redirect href="/sign-in" />;
-  return <Slot />;
-};
-
-export default _Layout;
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          borderTopLeftRadius: 50,
+          borderTopRightRadius: 50,
+          borderBottomLeftRadius: 50,
+          borderBottomRightRadius: 50,
+          marginHorizontal: 20,
+          height: 80,
+          position: "absolute",
+          bottom: 40,
+          backgroundColor: "white",
+          shadowColor: "#1a1a1a",
+          shadowOffset: {
+            width: 0,
+            height: 2,
+          },
+          shadowOpacity: 0.1,
+          shadowRadius: 4,
+          elevation: 5,
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ focused }) => (
+            <TabBarIcon focused={focused} icon={images.home} title="Home" />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="search"
+        options={{
+          title: "Search",
+          tabBarIcon: ({ focused }) => (
+            <TabBarIcon focused={focused} icon={images.search} title="Search" />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="cart"
+        options={{
+          title: "Cart",
+          tabBarIcon: ({ focused }) => (
+            <TabBarIcon focused={focused} icon={images.bag} title="Cart" />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          tabBarIcon: ({ focused }) => (
+            <TabBarIcon
+              focused={focused}
+              icon={images.person}
+              title="Profile"
+            />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
